f8154348ccd2201a0a9587b66755c76aa9eab146
Andrew Parisi <andrew.p.parisi@gmail.com>
2021-04-13 22:39:03 -0400

    Beginning of Documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
- import os
- import sys
- sys.path.insert(0, os.path.abspath('../..'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 # -- Project information -----------------------------------------------------
@@ -27,12 +27,9 @@ author = 'Andrew Parisi'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [rinoh.frontend.sphinx,
-              sphinx.ext.autodoc, 
-              sphinx.ext.autosectionlabel, 
-              sphinx.ext.autosummary, 
-              sphinx.ext.napolean
-]
+extensions = ["sphinx.ext.napolean",
+              "sphinx.ext.autodoc",
+             ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
